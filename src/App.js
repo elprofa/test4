51383e34c8a10b3b7d0a3dbc9e4fc6dc92dba7ac
elprofa wedo <wedoprofan14@gmail.com>
2021-01-07 14:45:36 +0100

    Mon commit de test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit elprofa <code>src/App.js</code> and save to reload.wedo
+          Edit elprofa je suis un ingenieur des travaux <code>src/App.js</code> and save to reload.wedo
         </p>
         <a
           className="App-link"
